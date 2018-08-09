bd78b38cf68243cee98f97b17fc7cb0b2bc7e8ff
Cesar Piangers <cesarpiangers@gmail.com>
2018-08-09 16:15:37 -0300

    Versão Alpha ( necessita ainda de tratamento de bugs)

--- BlockSpatial.py
+++ BlockSpatial.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4.QtCore import *
-from PyQt4.QtGui import QColor, QInputDialog, QLineEdit, QAction, QIcon
-from qgis.core import QGis, QgsMapLayerRegistry, QgsDistanceArea, QgsFeature, QgsPoint, QgsGeometry, QgsField, QgsVectorLayer, QgsExpressionContextUtils, QgsExpressionContextScope
-from qgis.gui import QgsMapToolEmitPoint, QgsRubberBand, QgsMapTool
+from PyQt4.QtGui import QColor, QInputDialog, QLineEdit, QAction, QIcon, QMessageBox
+from qgis.core import QGis, QgsMapLayerRegistry, QgsDistanceArea, QgsFeature, QgsPoint, QgsGeometry, QgsField, QgsVectorLayer, QgsExpressionContextUtils, QgsExpressionContextScope, QgsFeatureRequest
+from qgis.gui import QgsMapToolEmitPoint, QgsRubberBand, QgsMapTool, QgsMessageBar
 import resources_rc
 
 class BlockSpatial():
     
 
     def __init__(self, iface):
+
         self.iface = iface
 		
     def initGui(self): 
+
         self.layer = self.iface.activeLayer()
-        # cria uma ação que iniciará a configuração do plugin
         
         # Criação da action e da toolbar
         self.toolbar = self.iface.addToolBar("My_ToolBar")
@@ -27,61 +28,61 @@ class BlockSpatial():
         self.action.setCheckable(True)
         self.toolbar.addAction(self.action)
         self.isEditing = 0
-        self.layer.QgsVectorLayer.featureAdded.connect(self.run)
+
+        if self.layer:
+            self.iface.activeLayer().featureAdded.connect(self.run) # Sinal que chama a função e retorna o 'id'.
+        else:
+            pass
   
     def disconnect(self):
+        
         try:
             self.layer.featureAdded.disconnect(self.run)
         except:
             pass
+
     def unload(self):
+
+        del self.toolbar
+        try:
+            self.layer.featureAdded.disconnect(self.run)
+        except:
+            pass
         pass
 
     def unChecked(self):
+
         self.action.setCheckable(False)
         self.action.setCheckable(True)
      
 
-    def run(self,fId):
-          
-        varName = u'area_trabalho_poligono'
-        varSRID = QgsExpressionContextUtils.layerScope(self.layer).variable(varName)
+    def run(self,fid): # recebendo  id da funcao
         
-        if not(varSRID):
-            print (u"Variavel não encontrada.")
-        else:
-            print varSRID
+        
+    
+        
+        name = u'area_trabalho_poligono'
+        srid = QgsExpressionContextUtils.layerScope(self.layer).variable(name)
+        if not srid:
+            return
+        else: 
+            wkt = srid.replace('SRID=31982;','')
+            
+            mapcanvas = self.iface.mapCanvas()
+                    
+            geom = QgsGeometry()
+            geom = QgsGeometry.fromWkt(wkt)
+            
             for feat in self.layer.getFeatures():
-                self.layer.setSelectedFeatures(fId) # Teoricamente recebe o id da nova 
-                
-                if varSRID.geometry().intersects(feat.geometry()): # teoricamente testa se existe interseção
-
-                    print 'ok'
-                else:
-                    print 'não pertence'
-
-        #APOIO#
-  
-# layer = qgis.utils.iface.activeLayer()
-
-# # primeiro recurso da camada
-# elem1 = layer.getFeatures().next()
-# poly1 = elem1.geometry()
-# # segunda característica da camada
-# elem2 = layer.getFeatures().next()
-# poly2 = elem2.geometry()
-
-# poly1.intersects(poly2)
-
-# # então a linha de interseção
-# poly1.intersection(poly2)
-# # calcule os anéis
-# ring1= QgsGeometry.fromPolyline(poly1.asPolygon()[0])
-# ring2= QgsGeometry.fromPolyline(poly2.asPolygon()[0])
-# # predicate
-# ring1.intersects(ring2)
-# ring1.intersection(ring2)
+                if feat.id() == fid:
+                    if geom.intersects(feat.geometry()) == False:
+                        QMessageBox.information (self.iface.mainWindow() ,  u'ATENÇÃO!' ,  u"A aquisicão esta fora do limite de trabalho na camada " + self.layer.name())
+                        self.layer.deleteFeature(feat.id())
+                        return
+                    else:
+                        return
+                        
+            mapcanvas.refresh()
 
 
-		
-	
+
